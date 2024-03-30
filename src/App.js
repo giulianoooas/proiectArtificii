b42124f0dafe53ffa4572e08699b2e9ca9b14232
giulianoooas <florentingiulianodumitru@gmail.com>
2024-03-31 00:55:15 +0200

    fix redirection

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles/styles.css";
 import { Navbar } from "./components/navbar.component";
 import { Footer } from "./components/footer.component";
 import { Home } from "./components/home.component";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { About } from "./components/about.component";
 import { Services } from "./components/services.component";
 
@@ -16,6 +16,8 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
       <Footer />
