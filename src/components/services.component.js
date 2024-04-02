00bc3f8b8fd78d7b55cdf898a9b19b58d2cbf26b
giulianoooas <florentingiulianodumitru@gmail.com>
2024-04-02 22:45:31 +0300

    fix service problems

--- src/components/services.component.js
+++ src/components/services.component.js
@@ -1,4 +1,55 @@
 export const Services = () => {
+  const cards = [
+    {
+      title: "Service 1",
+      description: `This is a template that is great for small businesses. It
+    doesn't have too much fancy flare to it, but it makes a great
+    use of the standard Bootstrap core components. Feel free to use
+    this template for any project you want!This is a template that
+    is great for small businesses. It doesn't have too much fancy
+    flare to it, but it makes a great use of the standard Bootstrap
+    core components. Feel free to use this template for any project
+    you want`,
+      imageSrc: "https://dummyimage.com/900x400/dee2e6/6c757d.jpg",
+    },
+    {
+      title: "Service 2",
+      description: `This is a template that is great for small businesses. It
+    doesn't have too much fancy flare to it, but it makes a great
+    use of the standard Bootstrap core components. Feel free to use
+    this template for any project you want!This is a template that
+    is great for small businesses. It doesn't have too much fancy
+    flare to it, but it makes a great use of the standard Bootstrap
+    core components. Feel free to use this template for any project
+    you want`,
+      imageSrc: "https://dummyimage.com/900x400/dee2e6/6c757d.jpg",
+    },
+    {
+      title: "Service 3",
+      description: `This is a template that is great for small businesses. It
+    doesn't have too much fancy flare to it, but it makes a great
+    use of the standard Bootstrap core components. Feel free to use
+    this template for any project you want!This is a template that
+    is great for small businesses. It doesn't have too much fancy
+    flare to it, but it makes a great use of the standard Bootstrap
+    core components. Feel free to use this template for any project
+    you want`,
+      imageSrc: "https://dummyimage.com/900x400/dee2e6/6c757d.jpg",
+    },
+    {
+      title: "Service 4",
+      description: `This is a template that is great for small businesses. It
+    doesn't have too much fancy flare to it, but it makes a great
+    use of the standard Bootstrap core components. Feel free to use
+    this template for any project you want!This is a template that
+    is great for small businesses. It doesn't have too much fancy
+    flare to it, but it makes a great use of the standard Bootstrap
+    core components. Feel free to use this template for any project
+    you want`,
+      imageSrc: "https://dummyimage.com/900x400/dee2e6/6c757d.jpg",
+    },
+  ];
+
   return (
     <div className="container px-4 px-lg-5 lead">
       <div className="gx-4 gx-lg-5 align-items-center my-5 text-center">
@@ -12,114 +63,25 @@ export const Services = () => {
           </p>
         </div>
       </div>
-      <div className="card text-white bg-secondary my-5 text-center">
-        <div className="card-body">
-          <div className="row gx-4 gx-lg-5 align-items-center my-5">
-            <h1 className="mb-5">Service 1</h1>
-            <div className="col-lg-7">
-              <img
-                className="img-fluid rounded mb-4 mb-lg-0"
-                src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
-                alt="..."
-              />
-            </div>
-            <div className="col-lg-5 lead">
-              <p>
-                This is a template that is great for small businesses. It
-                doesn't have too much fancy flare to it, but it makes a great
-                use of the standard Bootstrap core components. Feel free to use
-                this template for any project you want!This is a template that
-                is great for small businesses. It doesn't have too much fancy
-                flare to it, but it makes a great use of the standard Bootstrap
-                core components. Feel free to use this template for any project
-                you want
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card text-white bg-secondary my-5 text-center">
-        <div className="card-body">
-          <div className="gx-4 gx-lg-5 align-items-center my-5 text-center kbd lead">
-            <h1 className="mb-5">Service 2</h1>
+      {cards.map((card) => (
+        <div className="card text-white bg-secondary my-5 text-center">
+          <div className="card-body">
             <div className="row gx-4 gx-lg-5 align-items-center my-5">
-              <div className="col-lg-5 lead">
-                <p>
-                  This is a template that is great for small businesses. It
-                  doesn't have too much fancy flare to it, but it makes a great
-                  use of the standard Bootstrap core components. Feel free to
-                  use this template for any project you want!This is a template
-                  that is great for small businesses. It doesn't have too much
-                  fancy flare to it, but it makes a great use of the standard
-                  Bootstrap core components. Feel free to use this template for
-                  any project you want
-                </p>
-              </div>
+              <h1 className="mb-5">{card.title}</h1>
               <div className="col-lg-7">
                 <img
                   className="img-fluid rounded mb-4 mb-lg-0"
-                  src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
+                  src={card.imageSrc}
                   alt="..."
                 />
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card text-white bg-secondary my-5 text-center">
-        <div className="card-body">
-          <div className="row gx-4 gx-lg-5 align-items-center my-5">
-            <h1 className="mb-5">Service 3</h1>
-            <div className="col-lg-7">
-              <img
-                className="img-fluid rounded mb-4 mb-lg-0"
-                src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
-                alt="..."
-              />
-            </div>
-            <div className="col-lg-5 lead">
-              <p>
-                This is a template that is great for small businesses. It
-                doesn't have too much fancy flare to it, but it makes a great
-                use of the standard Bootstrap core components. Feel free to use
-                this template for any project you want!This is a template that
-                is great for small businesses. It doesn't have too much fancy
-                flare to it, but it makes a great use of the standard Bootstrap
-                core components. Feel free to use this template for any project
-                you want
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card text-white bg-secondary my-5 text-center">
-        <div className="card-body">
-          <div className="gx-4 gx-lg-5 align-items-center my-5 text-center kbd lead">
-            <h1 className="mb-5">Service 4</h1>
-            <div className="row gx-4 gx-lg-5 align-items-center my-5">
               <div className="col-lg-5 lead">
-                <p>
-                  This is a template that is great for small businesses. It
-                  doesn't have too much fancy flare to it, but it makes a great
-                  use of the standard Bootstrap core components. Feel free to
-                  use this template for any project you want!This is a template
-                  that is great for small businesses. It doesn't have too much
-                  fancy flare to it, but it makes a great use of the standard
-                  Bootstrap core components. Feel free to use this template for
-                  any project you want
-                </p>
-              </div>
-              <div className="col-lg-7">
-                <img
-                  className="img-fluid rounded mb-4 mb-lg-0"
-                  src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
-                  alt="..."
-                />
+                <p>{card.description}</p>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
