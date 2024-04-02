ed2f34cc58a28816f677b19b391a13e946f5f36e
giulianoooas <florentingiulianodumitru@gmail.com>
2024-04-02 16:13:21 +0300

    add one more service on services page

--- src/components/services.component.js
+++ src/components/services.component.js
@@ -14,86 +14,111 @@ export const Services = () => {
       </div>
       <div className="card text-white bg-secondary my-5 text-center">
         <div className="card-body">
-          <div className="row gx-4 gx-lg-5 align-items-center my-5">        
-          <h1 className="mb-5">Service 1</h1>
-        <div className="col-lg-7">
-          <img
-            className="img-fluid rounded mb-4 mb-lg-0"
-            src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
-            alt="..."
-          />
+          <div className="row gx-4 gx-lg-5 align-items-center my-5">
+            <h1 className="mb-5">Service 1</h1>
+            <div className="col-lg-7">
+              <img
+                className="img-fluid rounded mb-4 mb-lg-0"
+                src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
+                alt="..."
+              />
+            </div>
+            <div className="col-lg-5 lead">
+              <p>
+                This is a template that is great for small businesses. It
+                doesn't have too much fancy flare to it, but it makes a great
+                use of the standard Bootstrap core components. Feel free to use
+                this template for any project you want!This is a template that
+                is great for small businesses. It doesn't have too much fancy
+                flare to it, but it makes a great use of the standard Bootstrap
+                core components. Feel free to use this template for any project
+                you want
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="col-lg-5 lead">
-          <p>
-            This is a template that is great for small businesses. It doesn't
-            have too much fancy flare to it, but it makes a great use of the
-            standard Bootstrap core components. Feel free to use this template
-            for any project you want!This is a template that is great for small
-            businesses. It doesn't have too much fancy flare to it, but it makes
-            a great use of the standard Bootstrap core components. Feel free to
-            use this template for any project you want
-          </p>
-        </div>
-        </div>
-      </div>
-      
-        
-        
       </div>
       <div className="card text-white bg-secondary my-5 text-center">
         <div className="card-body">
           <div className="gx-4 gx-lg-5 align-items-center my-5 text-center kbd lead">
-          <h1 className="mb-5">Service 2</h1>
-          <div className="row gx-4 gx-lg-5 align-items-center my-5">
-          <div className="col-lg-5 lead">
-          <p>
-            This is a template that is great for small businesses. It doesn't
-            have too much fancy flare to it, but it makes a great use of the
-            standard Bootstrap core components. Feel free to use this template
-            for any project you want!This is a template that is great for small
-            businesses. It doesn't have too much fancy flare to it, but it makes
-            a great use of the standard Bootstrap core components. Feel free to
-            use this template for any project you want
-          </p>
-        </div>
-          <div className="col-lg-7">
-          <img
-            className="img-fluid rounded mb-4 mb-lg-0"
-            src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
-            alt="..."
-          />
+            <h1 className="mb-5">Service 2</h1>
+            <div className="row gx-4 gx-lg-5 align-items-center my-5">
+              <div className="col-lg-5 lead">
+                <p>
+                  This is a template that is great for small businesses. It
+                  doesn't have too much fancy flare to it, but it makes a great
+                  use of the standard Bootstrap core components. Feel free to
+                  use this template for any project you want!This is a template
+                  that is great for small businesses. It doesn't have too much
+                  fancy flare to it, but it makes a great use of the standard
+                  Bootstrap core components. Feel free to use this template for
+                  any project you want
+                </p>
+              </div>
+              <div className="col-lg-7">
+                <img
+                  className="img-fluid rounded mb-4 mb-lg-0"
+                  src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
+                  alt="..."
+                />
+              </div>
+            </div>
           </div>
-         </div>
-        </div>
         </div>
       </div>
       <div className="card text-white bg-secondary my-5 text-center">
         <div className="card-body">
-          <div className="row gx-4 gx-lg-5 align-items-center my-5">        
-          <h1 className="mb-5">Service 3</h1>
-        <div className="col-lg-7">
-          <img
-            className="img-fluid rounded mb-4 mb-lg-0"
-            src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
-            alt="..."
-          />
-        </div>
-        <div className="col-lg-5 lead">
-          <p>
-            This is a template that is great for small businesses. It doesn't
-            have too much fancy flare to it, but it makes a great use of the
-            standard Bootstrap core components. Feel free to use this template
-            for any project you want!This is a template that is great for small
-            businesses. It doesn't have too much fancy flare to it, but it makes
-            a great use of the standard Bootstrap core components. Feel free to
-            use this template for any project you want
-          </p>
-        </div>
+          <div className="row gx-4 gx-lg-5 align-items-center my-5">
+            <h1 className="mb-5">Service 3</h1>
+            <div className="col-lg-7">
+              <img
+                className="img-fluid rounded mb-4 mb-lg-0"
+                src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
+                alt="..."
+              />
+            </div>
+            <div className="col-lg-5 lead">
+              <p>
+                This is a template that is great for small businesses. It
+                doesn't have too much fancy flare to it, but it makes a great
+                use of the standard Bootstrap core components. Feel free to use
+                this template for any project you want!This is a template that
+                is great for small businesses. It doesn't have too much fancy
+                flare to it, but it makes a great use of the standard Bootstrap
+                core components. Feel free to use this template for any project
+                you want
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      
-        
-        
+      <div className="card text-white bg-secondary my-5 text-center">
+        <div className="card-body">
+          <div className="gx-4 gx-lg-5 align-items-center my-5 text-center kbd lead">
+            <h1 className="mb-5">Service 4</h1>
+            <div className="row gx-4 gx-lg-5 align-items-center my-5">
+              <div className="col-lg-5 lead">
+                <p>
+                  This is a template that is great for small businesses. It
+                  doesn't have too much fancy flare to it, but it makes a great
+                  use of the standard Bootstrap core components. Feel free to
+                  use this template for any project you want!This is a template
+                  that is great for small businesses. It doesn't have too much
+                  fancy flare to it, but it makes a great use of the standard
+                  Bootstrap core components. Feel free to use this template for
+                  any project you want
+                </p>
+              </div>
+              <div className="col-lg-7">
+                <img
+                  className="img-fluid rounded mb-4 mb-lg-0"
+                  src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
+                  alt="..."
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
